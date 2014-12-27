495b864a85c997c382e59f9a7de5fe1f2d7df977
Justin Meyer <justinbmeyer@gmail.com>
2014-12-26 20:28:50 -0600

    added globalBrowser

---
+++ browser/traceur.js
@@ -0,0 +1,17 @@
+var globalTraceur = require('traceur/bin/traceur');
+
+if(!globalTraceur) {
+    globalTraceur = {
+        System: System,
+        Traceur: Traceur,
+        $traceurRuntime: $traceurRuntime
+    };
+}
+
+// traceur is a module and thus frozen.
+module.exports = {
+  __proto__: globalTraceur.traceur,
+  get require() {
+    return require('traceur/src/node/require.js');
+  }
+};
