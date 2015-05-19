d781e361581f09333122ed7f2c19cd0281c721b5
Matthew Phillips <matthew@matthewphillips.info>
2015-05-19 14:59:42 -0400

    Transpile globals to System.register
    This changes globals to use a System.register format. For
    experimentation only.

--- test/tests/expected/global_amd_with_format.js
+++ test/tests/expected/global_amd_with_format.js
@@ -1,4 +1,7 @@
-System.define('global', 'var GLOBAL = "I don\'t like \\"Quotes\\"";', {
-    'address': 'global',
-    'metadata': { 'format': 'global' }
+System.register('global', [], false, function (__require, __exports, __module) {
+    System.get('@@global-helpers').prepareGlobal(__module.id, []);
+    (function () {
+        var GLOBAL = 'I don\'t like "Quotes"';
+    }.call(System.global));
+    return System.get('@@global-helpers').retrieveGlobal(__module.id, false);
 });
