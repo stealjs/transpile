75814a98d7c26278a65a7504406227ca27223069
Matthew Phillips <matthew@matthewphillips.info>
2016-01-21 08:37:30 -0500

    Upgrade to Babel 6

--- test/tests/expected/es62_amd_sm.js
+++ test/tests/expected/es62_amd_sm.js
@@ -1,17 +1,17 @@
 define([
     'exports',
     'basics/amdmodule'
-], function (exports, _basicsAmdmodule) {
+], function (exports, _amdmodule) {
     'use strict';
     Object.defineProperty(exports, '__esModule', { value: true });
+    exports.__useDefault = undefined;
+    var _amdmodule2 = _interopRequireDefault(_amdmodule);
     function _interopRequireDefault(obj) {
-        return obj && obj.__esModule ? obj : { 'default': obj };
+        return obj && obj.__esModule ? obj : { default: obj };
     }
-    var _amdMod = _interopRequireDefault(_basicsAmdmodule);
-    exports['default'] = {
-        amdModule: _amdMod['default'],
+    exports.default = {
+        amdModule: _amdmodule2.default,
         name: 'es6Module'
     };
-    var __useDefault = true;
-    exports.__useDefault = __useDefault;
+    var __useDefault = exports.__useDefault = true;
 }); //# sourceMappingURL=es62_amd_sm.js.map
--- test/tests/expected/es62_cjs_sm.js
+++ test/tests/expected/es62_cjs_sm.js
@@ -1,13 +1,13 @@
 'use strict';
 Object.defineProperty(exports, '__esModule', { value: true });
+exports.__useDefault = undefined;
+var _amdmodule = require('basics/amdmodule');
+var _amdmodule2 = _interopRequireDefault(_amdmodule);
 function _interopRequireDefault(obj) {
-    return obj && obj.__esModule ? obj : { 'default': obj };
+    return obj && obj.__esModule ? obj : { default: obj };
 }
-var _basicsAmdmodule = require('basics/amdmodule');
-var _basicsAmdmodule2 = _interopRequireDefault(_basicsAmdmodule);
-exports['default'] = {
-    amdModule: _basicsAmdmodule2['default'],
+exports.default = {
+    amdModule: _amdmodule2.default,
     name: 'es6Module'
 };
-var __useDefault = true;
-exports.__useDefault = __useDefault; //# sourceMappingURL=es62_cjs_sm.js.map
+var __useDefault = exports.__useDefault = true; //# sourceMappingURL=es62_cjs_sm.js.map
