693a06c79dcf3ea6d660954ea27e8aaf0bd11dad
Matthew Phillips <matthew@matthewphillips.info>
2017-10-17 14:25:10 -0400

    Pass require, exports, module into global/dirname wrapper function.
    Passing these values into the wrapper ensures that they are not masked
    by variable declarations within the body of the module.
    
    Closes #124

--- test/tests/expected/cjs_global_dirname.js
+++ test/tests/expected/cjs_global_dirname.js
@@ -1,8 +1,8 @@
 define(function (require, exports, module) {
-    (function (global, __dirname) {
+    (function (global, __dirname, require, exports, module) {
         var Math = global.Math;
         var p = __dirname + '/foo';
     }(function () {
         return this;
-    }(), '/'));
+    }(), '/', require, exports, module));
 });
--- test/tests/expected/cjs_global_without_dot.js
+++ test/tests/expected/cjs_global_without_dot.js
@@ -1,10 +1,10 @@
 define(function (require, exports, module) {
-    (function (global) {
+    (function (global, require, exports, module) {
         var foo = getFromGlobal(global, 'foo');
         function getFromGlobal(global, prop) {
             return global[prop];
         }
     }(function () {
         return this;
-    }()));
+    }(), require, exports, module));
 });
